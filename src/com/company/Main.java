6652c12fe5f8e140dc5e7f913a4848714fb531f7
nazar <nazarsmuk94@gmail.com>
2023-04-17 21:27:24 +0300

    text

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         //Using builder to get the object in a single line of code and
         //without any inconsistent state or arguments management issues
         //commit 1
+        //commit 2
         Computer comp = new Computer.ComputerBuilder(
             "500 GB", "2 GB").setBluetoothEnabled(true)
             .setGraphicsCardEnabled(true).build();
